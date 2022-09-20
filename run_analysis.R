8846b2880044dc9bf8af30fd42bf9e9d2eb10aef
ZhiYLu <86559460+ZhiYLu@users.noreply.github.com>
2022-09-20 23:11:54 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,13 +2,11 @@ library(dplyr)
 
 filename <- "Coursera_DS3_Final.zip"
 
-# Checking if archieve already exists.
 if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileURL, filename, method="curl")
 }  
 
-# Checking if folder exists
 if (!file.exists("UCI HAR Dataset")) { 
   unzip(filename) 
 }
@@ -32,23 +30,23 @@ TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("st
 TidyData$code <- activities[TidyData$code, 2]
 
 names(TidyData)[2] = "activity"
-names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
-names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
-names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-names(TidyData)<-gsub("^t", "Time", names(TidyData))
-names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
-names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+names(TidyData) <- gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData) <- gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData) <- gsub("BodyBody", "Body", names(TidyData))
+names(TidyData) <- gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData) <- gsub("^t", "Time", names(TidyData))
+names(TidyData) <- gsub("^f", "Frequency", names(TidyData))
+names(TidyData) <- gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData) <- gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData) <- gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData) <- gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData) <- gsub("angle", "Angle", names(TidyData))
+names(TidyData) <- gsub("gravity", "Gravity", names(TidyData))
 
 FinalData <- TidyData %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
-write.table(FinalData, "FinalData.txt", row.name=FALSE)
+write.table(FinalData, "TidyData.txt", row.name=FALSE)
 
 str(FinalData)
 FinalData
